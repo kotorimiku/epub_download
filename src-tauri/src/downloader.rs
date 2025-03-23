5921c92cd85288a096300a1721c7a9b9fbba983c
Kotorimiku <kotorimiku@users.noreply.github.com>
2025-03-24 02:15:30 +0800

    update

--- src-tauri/src/downloader.rs
+++ src-tauri/src/downloader.rs
@@ -11,8 +11,9 @@ use std::thread::sleep;
 // use reqwest::cookie::Jar;
 use crate::utils;
 use reqwest::blocking::Client;
-use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, ACCEPT_LANGUAGE, COOKIE, USER_AGENT};
+use reqwest::header::{HeaderValue, ACCEPT};
 use scraper::{Html, Selector};
+use crate::client::*;
 
 const ERROR_IMG: [&str; 8] = [
     "https://www.xlcx996.xyz/image/novel/sister01.jpg", // 3273/167199.html
@@ -24,56 +25,6 @@ const ERROR_IMG: [&str; 8] = [
     "https://img1.imgtp.com/2022/07/27/3kRju45s.png",   // 3342/169587_3.html
     "../Images/00018.jpeg",                             // 3382/249127.html
 ];
-
-fn get_headers(referer: &str, cookie: &str) -> HeaderMap {
-    let mut headers = HeaderMap::new();
-    headers.insert(USER_AGENT, HeaderValue::from_static("Mozilla/5.0 (Linux; Android 11; M2102J20SG) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Mobile Safari/537.36 EdgA/97.0.1072.78"));
-    headers.insert(
-        ACCEPT_LANGUAGE,
-        HeaderValue::from_static(
-            "en,zh-HK;q=0.9,zh-TW;q=0.8,zh-CN;q=0.7,zh;q=0.6,en-GB;q=0.5,en-US;q=0.4",
-        ),
-    );
-    headers.insert(ACCEPT, HeaderValue::from_static(r"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"));
-    headers.insert(COOKIE, HeaderValue::from_str(cookie).unwrap());
-    headers.insert(
-        "Referer",
-        HeaderValue::from_str(&(referer.to_string() + "/novel/4353/250879.html")).unwrap(),
-    );
-    headers.insert(
-        "accept-encoding",
-        HeaderValue::from_static("gzip, deflate, br, zstd"),
-    );
-    headers.insert("priority", HeaderValue::from_static("u=0, i"));
-    headers.insert(
-        "sec-ch-ua",
-        HeaderValue::from_static(
-            "\"Microsoft Edge\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
-        ),
-    );
-    headers.insert("sec-ch-ua-mobile", HeaderValue::from_static("?1"));
-    headers.insert(
-        "sec-ch-ua-platform",
-        HeaderValue::from_static("\"Android\""),
-    );
-    headers.insert("sec-fetch-dest", HeaderValue::from_static("document"));
-    headers.insert("upgrade-insecure-requests", HeaderValue::from_static("1"));
-    headers.insert("sec-fetch-user", HeaderValue::from_static("?1"));
-    headers.insert("sec-fetch-site", HeaderValue::from_static("same-origin"));
-    headers.insert("sec-fetch-mode", HeaderValue::from_static("navigate"));
-    headers.insert(
-        "Referrer-Policy",
-        HeaderValue::from_static("strict-origin-when-cross-origin"),
-    );
-    headers
-}
-
-pub fn get_client(referer: &str, cookie: &str) -> Client {
-    // 创建一个 Cookie Jar
-    let headers = get_headers(referer, cookie);
-    // let jar = Arc::new(Jar::default());
-    Client::builder().default_headers(headers).build().unwrap()
-}
 
 fn get_html(
     url: &str,
@@ -664,7 +615,7 @@ impl Downloader {
             );
         }
         if let Ok(response) = client.send() {
-            let length = response.content_length().unwrap_or(0);
+            // let length = response.content_length().unwrap_or(0);
             // let code = response.status().as_u16();
             let data = response.bytes().unwrap();
 
@@ -674,9 +625,9 @@ impl Downloader {
             //     return Vec::new();
             // }
 
-            if length != data.len() as u64 {
-                return Err("插图下载失败".to_string());
-            }
+            // if length != data.len() as u64 {
+            //     return Err("插图下载失败".to_string());
+            // }
 
             return match utils::img_to_jpg(data.to_vec()) {
                 Ok(data) => Ok(data),
